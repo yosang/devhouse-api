93946890d6512d2cdbb78257a06288c84506a1a1
Yosmel Chiang <yosmelchiang@gmail.com>
2026-03-20 12:23:12 +0100

    feat: implement validations to CreateDeveloperDTO

--- DTOs/Developers/CreateDeveloperDTO.cs
+++ DTOs/Developers/CreateDeveloperDTO.cs
@@ -1,11 +1,27 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace devhouse.DTOs;
 
 public class CreateDeveloperDTO
 {
+    [Required]
     public string? Firstname { get; set; }
+
+    [Required]
     public string? Lastname { get; set; }
+
+    [EmailAddress]
     public string? Email { get; set; }
+
+    [MinLength(8)]
     public string? Password { get; set; }
+
+    [Required]
+    [Range(1, 3)]
     public int TeamId { get; set; }
+
+    [Range(1, 3)]
+    [Required]
     public int RoleId { get; set; }
 }
