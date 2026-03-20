ec9bdc81c2358b71ec43ba50e8ac1fdf93fdea09
Yosmel Chiang <yosmelchiang@gmail.com>
2026-03-20 14:11:06 +0100

    feat: add sample request body to login endpoint

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -11,7 +11,21 @@ public class AuthController : ControllerBase
 
     public AuthController(AuthService service) => _service = service;
 
-    [HttpPost]
+    /// <summary>Login for a JWT token</summary>
+    /// <remarks>
+    /// Sample Request:
+    /// 
+    ///     {
+    ///         "email": "dev@dev.com",
+    ///         "password": "password1234"
+    ///     }
+    /// 
+    /// </remarks>
+    /// <param name="loginDTO"></param>
+    /// <returns>JWT Token on successful login</returns>
+    [HttpPost("/api/[controller]/login")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
     public async Task<ActionResult<string>> Login(LoginDTO loginDTO)
     {
         var (authenticated, token) = await _service.Authenticate(loginDTO.Email, loginDTO.Password);
